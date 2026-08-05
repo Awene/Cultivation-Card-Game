56f1eb2fe7a3ca6eb89a8db6e8677ed823c73fb8
Awene <1170075937@qq.com>
2026-08-05 12:02:53 +0800

    更新前端指针至自动构建 v1.0.37

--- 脚本/【本格修仙】创意工坊.js
+++ 脚本/【本格修仙】创意工坊.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const workshopUrl =
-    'https://testingcf.jsdelivr.net/gh/Awene/tavern_helper_template-main@v1.0.36/dist/创意工坊/index.js';
+    'https://testingcf.jsdelivr.net/gh/Awene/tavern_helper_template-main@v1.0.37/dist/创意工坊/index.js';
   const positionKey = 'cultivation-workshop-launcher-position';
   const themeKey = 'rb-theme';
   const launcherSize = () => (window.parent.innerWidth < 600 ? 46 : 54);
